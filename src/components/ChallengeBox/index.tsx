6918e90dfe8cd451483d60463121e231e763a81c
Pedro <pedropaulo3521@gmail.com>
2021-02-25 14:10:15 -0300

    Terminada a estilização do ChallengBox

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,17 +1,44 @@
-import { ChallengeBoxContainer, ChallengeBoxNotActive } from '../../styles/components/ChallengeBox.module';
+import { 
+    ChallengeBoxActive, ChallengeBoxContainer, ChallengeBoxNotActive, FailedButton, 
+    SucceedButton 
+    }
+ from '../../styles/components/ChallengeBox.module';
 
-export default function ChallengeBox(){
-    return(
+export default function ChallengeBox() {
+
+    const hasChallenge = true;
+
+    return (
         <ChallengeBoxContainer>
-            <ChallengeBoxNotActive>
-                <strong>
-                    Finalize um ciclo para receber um desafio!
-                </strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level up"/>
-                    Avance de level completando desafios!
-                </p>
-            </ChallengeBoxNotActive>
+            {
+                hasChallenge ?
+                   <ChallengeBoxActive>
+                       <header>Ganhe vários xp's</header>
+                       <main>
+                           <img src="icons/body.svg" alt=""/>
+                           <strong>Novo desafio!</strong>
+                           <p>Vá fazer uma sessão de alongamento.</p>
+                       </main>
+                       <footer>
+                           <SucceedButton>
+                               Sucesso
+                           </SucceedButton>
+                           <FailedButton>
+                               Fracasso
+                           </FailedButton>
+                       </footer>
+                   </ChallengeBoxActive>
+                    :
+                    <ChallengeBoxNotActive>
+                        <strong>
+                            Finalize um ciclo para receber um desafio!
+                         </strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="Level up" />
+                            Avance de level completando desafios!
+                        </p>
+                    </ChallengeBoxNotActive>
+            }
         </ChallengeBoxContainer>
     )
 }
--- src/styles/components/ChallengeBox.module.tsx
+++ src/styles/components/ChallengeBox.module.tsx
@@ -14,13 +14,16 @@ export const ChallengeBoxContainer = styled.div`
     justify-content: center;
 
     text-align: center;
+
+    & > div{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        height: 100%;
+    }
 `
 export const ChallengeBoxNotActive = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    height: 50%;
 
     & strong{
         font-size: 1.5rem;
@@ -34,4 +37,72 @@ export const ChallengeBoxNotActive = styled.div`
         justify-content: center;
         max-width: 70%;
     }
+`
+export const ChallengeBoxActive = styled.div`
+    
+    & header{
+        color: var(--blue);
+        font-weight: 600;
+        font-size: 1.25rem;
+        padding: 0 2rem 1.5rem;
+        border-bottom: 1px solid var(--gray-line);
+    }
+
+    & main{
+        flex: 1;
+
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    & main strong{
+        font-size: 2rem;
+        font-weight: 600;
+        color: var(--title);
+        margin: 1.5rem 0 1rem;
+    }
+
+    & main p{
+        line-height: 1.5rem;
+    }
+
+    & footer{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 1rem;
+
+        width: 100%;
+    }
+
+    & footer button{
+        height: 3rem;
+        
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border: 0;
+        border-radius: 5px;
+
+        color: var(--white);
+
+        font-size: 1rem;
+        font-weight: 600;
+
+        transition: .2s;
+    }
+
+    & footer button:hover{
+        filter: brightness(0.9);
+    }
+`
+
+export const SucceedButton = styled.button`
+    background: var(--green);
+`
+
+export const FailedButton = styled.button`
+    background: var(--red);
 `
