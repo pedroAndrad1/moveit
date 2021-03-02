e1e4533d5fca00787b91e6bcdbe66d6053ebfe8b
Pedro <pedropaulo3521@gmail.com>
2021-03-01 22:48:20 -0300

    Adicionada animação ao LevelUpModal com o framer motion

--- src/components/LevelUpModal/LevelUpModal.tsx
+++ src/components/LevelUpModal/LevelUpModal.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useContext } from "react";
 import { ChallengesContext } from "../../Contexts/ChallengesContext";
 import { Overlay, ModalContainer} from "../../styles/components/LevelUpModal.module";
@@ -7,8 +8,19 @@ export default function LevelUpModal(){
     const {level, closeLevelUpModal} = useContext(ChallengesContext);
     
     return(
-        <Overlay>
-            <ModalContainer>
+        <Overlay
+            as={motion.div}
+            transition={{ delay: 0, duration: 0.5 }}
+            variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0},
+            }}
+            initial="hidden"
+            animate="show"
+        >
+            <ModalContainer
+                 
+            >
                 <header>{level}</header>
 
                 <strong>Parabéns!</strong>
